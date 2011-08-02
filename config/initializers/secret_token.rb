42f2264c1d35a15aed972d331b062959dc53dad0
Jodas <jodas@jodas-M14xR1.(none)>
2011-08-02 21:39:07 +0200

    initial push (rails skeleton)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestBlog::Application.config.secret_token = '467c96a922b9a5496f12d5558e84d3a1f6fcf9bf16d9607f9b100acba915971ec3409d7308bfe7f5d35d2ddedd994be54c9e38db81a23f7747a77c557094b33f'
